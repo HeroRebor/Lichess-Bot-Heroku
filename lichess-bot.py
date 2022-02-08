04581a67b8799f49e497fe4702a51ce4c6e93306
SriMethan <77402282+SriMethan@users.noreply.github.com>
2022-02-08 08:20:20 +0530

    Update lichess-bot.py

--- lichess-bot.py
+++ lichess-bot.py
@@ -229,19 +229,7 @@ def play_game(li, game_id, control_queue, engine_factory, user_profile, config, 
     engine.get_opponent_info(game)
     conversation = Conversation(game, engine, li, __version__, challenge_queue)
     
-    class SendLine:
-        def __init__(self, room):
-            self.room = room
-    opponent = game.black.name if game.white.name == user_profile["username"] else game.white.name
-    CONFIG = load_config(args.config or "./config.yml")
-    li = lidraughts.Lidraughts(CONFIG["token"], CONFIG["url"], __version__)
-    user_profile = li.get_profile()
-    username = user_profile["username"]
-    conversation.send_reply(SendLine('player'), f'{username} running github.com/srimethan/lidraughts-bot-heroku')
-    conversation.send_reply(SendLine('spectator'), f'{username} running github.com/srimethan/lidraughts-bot-heroku')
-    conversation.send_reply(SendLine('player'), f'Good Luck!')
-    conversation.send_reply(SendLine('spectator'), f'Good Luck!')
-    
+   
     variant=game.perf_name
     
     if variant=="standard":
